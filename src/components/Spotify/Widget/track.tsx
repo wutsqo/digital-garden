72904d2f860d73033ca034eac14ea92ff30e8cb0
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2022-02-11 12:34:41 +0700

    🎧 Refactor track widget

--- src/components/Spotify/Widget/track.tsx
+++ src/components/Spotify/Widget/track.tsx
@@ -15,49 +15,37 @@ interface TrackProps {
 const TrackWidget: FC<TrackProps> = (props) => {
   const { name, album, artists, external_urls, pos } = props
 
+  if (!name)
+    return (
+      <div className="flex w-full animate-pulse h-16">
+        <div className="bg-dark-cream dark:bg-emerald-900 w-20 h-full" />
+        <div className="w-full pl-2">
+          <div className="bg-dark-cream dark:bg-emerald-900 w-8 h-3 mt-2" />
+          <div className="bg-dark-cream dark:bg-emerald-900 w-36 h-4 mt-1" />
+          <div className="bg-dark-cream dark:bg-emerald-900 w-24 h-3 mt-1" />
+        </div>
+      </div>
+    )
+
   return (
     <a
-      href={external_urls ? external_urls?.spotify : "#"}
+      href={external_urls?.spotify}
       target="_blank"
       rel="noopener noreferrer"
-      className={`flex w-full hover:bg-dark-cream dark:hover:bg-emerald-900 h-16 hover:shadow ${
-        !name && "animate-pulse"
-      } `}
+      className="flex w-full hover:bg-dark-cream dark:hover:bg-emerald-900 h-16 hover:shadow"
     >
-      <div className={`flex-shrink-0 h-16 w-16`}>
-        {name ? (
-          <img
-            src={album?.images[1]?.url}
-            alt={name}
-            className="h-full w-full"
-          />
-        ) : (
-          <div className="bg-dark-cream dark:bg-emerald-900  w-full h-full"></div>
-        )}
+      <div className="flex-shrink-0 h-16 w-16">
+        <img src={album?.images[1]?.url} alt={name} className="h-full w-full" />
       </div>
-      <div className={`w-full ${name ? "pl-4" : "pl-2"}`}>
+      <div className="w-full pl-4">
         <div
-          className={`${
-            name
-              ? "w-full mt-1.5"
-              : "bg-dark-cream dark:bg-emerald-900 w-8 h-3 mt-2"
-          }`}
+          className="w-full mt-1.5"
           style={{ fontSize: "0.625rem", lineHeight: "0.75rem" }}
         >
-          {name && `#${pos}`}
+          {`#${pos}`}
         </div>
-        <div
-          className={`font-medium text-sm ${
-            name ? "w-full" : "bg-dark-cream dark:bg-emerald-900 w-36 h-4  mt-1"
-          }`}
-        >
-          {name}
-        </div>
-        <div
-          className={`font-light text-sm ${
-            name ? "w-full" : "bg-dark-cream dark:bg-emerald-900 w-24 h-3 mt-1"
-          }`}
-        >
+        <div className="font-medium text-sm w-full">{name}</div>
+        <div className="font-light text-sm w-full">
           {artists?.length > 0 && artists[0]?.name}
         </div>
       </div>
