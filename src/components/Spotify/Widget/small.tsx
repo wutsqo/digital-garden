8f5cea09494456b78444349d87c1bc70f501f27e
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2022-03-06 23:24:20 +0700

    🎶 Update Spotify Widget

--- src/components/Spotify/Widget/small.tsx
+++ src/components/Spotify/Widget/small.tsx
@@ -4,10 +4,12 @@ import { FaSpotify } from "@react-icons/all-files/fa/FaSpotify"
 
 const SpotifyWidgetSmall: FC = () => {
   const [data, setData] = useState<any>({})
+  const [type, setType] = useState<any>({})
 
   const fetchData = () => {
     axios.get("/api/spotify-recent").then((response) => {
       setData(response.data?.item)
+      setType(response.data?.type)
     })
   }
 
@@ -23,47 +25,53 @@ const SpotifyWidgetSmall: FC = () => {
   }, [])
 
   return (
-    <div className="relative p-1 flex items-center max-w-xs text-sm">
-      <FaSpotify
-        className="-ml-1 text-2xl flex-shrink-0"
-        style={{ color: "#1DB954" }}
-      />
-      <div className="max-h-12 px-2 ml-2 border-l-2 border-black dark:border-emerald-900 items-center h-8 flex">
-        <div>
-          {data.id ? (
-            <div
-              className="max-w-xs md:w-96 font-semibold truncate leading-tight fade-in"
-              key={data.id}
-            >
-              <a
-                href={data.album.external_urls.spotify}
-                className="hover:underline"
-                target="_blank"
-                rel="noopener noreferrer"
+    <>
+      <div className="text-xs mb-1">
+        {type === "current" ? "currently" : "recently"} listening to:
+      </div>
+      <div className="relative p-1 pl-0 flex items-center max-w-xs text-sm">
+        <img
+          src={data?.album?.images[2].url}
+          alt={data.name}
+          className="h-10 w-10"
+        />
+        <div className="max-h-12 px-2 items-center h-8 flex">
+          <div>
+            {data.id ? (
+              <div
+                className="max-w-xs md:w-96 font-semibold truncate leading-tight fade-in"
+                key={data.id}
               >
-                {data.name}
-              </a>
-            </div>
-          ) : (
-            <div className="h-4 w-36 ph-child" />
-          )}
-          {data?.id ? (
-            <div className="font-light fade-in" key={data.artists[0].id}>
-              <a
-                href={data.artists[0].external_urls.spotify}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:underline"
-              >
-                {data.artists[0].name}
-              </a>
-            </div>
-          ) : (
-            <div className="h-4 w-32 ph-child mt-1" />
-          )}
+                <a
+                  href={data.album.external_urls.spotify}
+                  className="hover:underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {data.name}
+                </a>
+              </div>
+            ) : (
+              <div className="h-4 w-36 ph-child" />
+            )}
+            {data?.id ? (
+              <div className="font-light fade-in" key={data.artists[0].id}>
+                <a
+                  href={data.artists[0].external_urls.spotify}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:underline"
+                >
+                  {data.artists[0].name}
+                </a>
+              </div>
+            ) : (
+              <div className="h-4 w-32 ph-child mt-1" />
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
