f37007efaeb2a65af1d14f197965ff2a1c66b135
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2022-04-24 23:49:04 +0700

    ✨ Add page transition

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,23 @@
 import "./src/styles/global.css"
+import React from "react"
+import { AnimatePresence } from "framer-motion"
+
+export const wrapPageElement = ({ element }) => (
+  <AnimatePresence exitBeforeEnter>{element}</AnimatePresence>
+)
+
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  const TRANSITION_DELAY = 0.3 * 1000 * 2
+
+  if (location.action === "PUSH") {
+    window.setTimeout(() => window.scrollTo(0, 0), TRANSITION_DELAY)
+  } else {
+    const savedPosition = getSavedScrollPosition(location) || [0, 0]
+    window.setTimeout(() => window.scrollTo(...savedPosition), TRANSITION_DELAY)
+  }
+
+  return false
+}
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-
+import { motion } from "framer-motion"
 import Header from "./components/header"
 import { Gradient1, Gradient2 } from "./components/gradients"
 
@@ -34,11 +34,32 @@ export const Layout: React.FC<DataProps> = ({
         <Gradient1 />
         <Gradient2 />
         <main className="container mx-auto w-full min-h-screen pt-16 p-4 z-10 relative">
-          {children}
+          <motion.main
+            initial={{
+              opacity: 0,
+              y: 200,
+            }}
+            animate={{
+              opacity: 1,
+              y: 0,
+            }}
+            exit={{
+              opacity: 0,
+              y: -200,
+            }}
+            transition={{
+              type: "spring",
+              mass: 0.35,
+              stiffness: 75,
+              duration: 0.3,
+            }}
+          >
+            {children}
+          </motion.main>
         </main>
         {!noFooter && (
           <footer className="container mx-auto py-24 text-center text-sm z-50 relative">
-            &copy; 2022 Made with 💜 by Wutsqo
+            &copy;2022 Made with 💜 by Wutsqo
           </footer>
         )}
       </div>
